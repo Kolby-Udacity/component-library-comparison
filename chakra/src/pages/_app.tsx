a72eb24cf468f467245076b39ce5a398b5b6003c
Kolby-Udacity <kolby.sisk@udacity.com>
2022-01-18 10:59:13 -0600

    feat: initial chakra setup

---
+++ chakra/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from 'next/app'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { theme } from '../theme'
+
+import 'normalize.css/normalize.css'
+import '../global-styles.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
