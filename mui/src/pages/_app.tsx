f40ad353659749781f7c362deae0e17f068c0489
Kolby-Udacity <kolby.sisk@udacity.com>
2022-01-19 15:13:05 -0600

    chore: add MUI cssbaseline

--- mui/src/pages/_app.tsx
+++ mui/src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import type { AppProps } from 'next/app'
 
+import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 
 import theme from '../theme'
 
-import 'normalize.css/normalize.css'
 import '../global-styles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   )
